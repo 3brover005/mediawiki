d2d990df3390a95b830ad5727e2a0ba8daf080c1
James Ridge <james@3bonboarding.com>
2023-08-16 12:42:16 +0300

    theme update

--- LocalSettings.php
+++ LocalSettings.php
@@ -129,7 +129,7 @@ $wgGroupPermissions['*']['edit'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector-2022";
+$wgDefaultSkin = "vector";
 wfLoadSkin( 'Vector' );
 
 # End of automatically generated settings.
